f90f45f09131594eb050f64ef07e3ecec181da84
Javier Guerra <javier@guerrag.com>
2011-12-10 07:36:39 -0500

    remove old test

--- tests/base_test.lua
+++ tests/base_test.lua
@@ -4,18 +4,7 @@ strict.off()
 
 
 context('lglib', function ()
-	context('table', function ()
-		test('equal', strict.wrap(function ()
-			local a = {2, 3, 5, x = 12, y = '12', z = "xvvv"}
-			local b = {2, 3, 5, x = 12, y = '12', z = "xvvv"}
-			local c =  {2, 4, 5, x = 12, y = '12', z = "vvvv"}
-
-			assert_true(table.equal(a, b))
-			assert_false(table.equal(b, c))
-			assert_false(table.equal(a, c))
-
-		end))
-
+	context('base', function ()
 		test ('typename', strict.wrap(function ()
 			assert_equal ('List', typename(List))
 		end))
