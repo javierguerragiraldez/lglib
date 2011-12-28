dc0405c1fbfa816ad2150c9dc74dddad8abdf87f
Javier Guerra <javier@guerrag.com>
2011-12-27 22:04:22 -0500

    start Dict tests

--- tests/dict_test.lua
+++ tests/dict_test.lua
@@ -1,18 +1,38 @@
+-- require 'lglib'
+--
+-- Dict = require 'lglib.dict'
+--
+-- a = Dict {
+-- 	x = 'xxxxxx',
+-- 	y = '34343434',
+-- 	z = 'zcvcvc',
+-- 	g = '4444444',
+-- 	h = '34343aaa',
+--
+-- }
+--
+-- ptable(a:keys())
+-- ptable(a:values())
+-- print(a:hasKey('x'))
+-- print(a:hasKey('xx'))
+-- print(a:isEmpty())
+--
+--
+local strict = require 'lglib.strict'
 require 'lglib'
-
 Dict = require 'lglib.dict'
+strict.off()
 
-a = Dict {
-	x = 'xxxxxx',
-	y = '34343434',
-	z = 'zcvcvc',
-	g = '4444444',
-	h = '34343aaa',
+context ('Dict', function ()
 
-}
+	test ('new', strict.wrap(function ()
+		local t = Dict()
+		assert_true (types(t,'table'))
+		local a = Dict { 1,2,3,4,5,6,7, [156]='wayout', x=123, y="343434"}
+		assert_true (table.equal({x=123,y="343434",[156]='wayout'}, a))
+		assert_nil (a[1])
+		assert_nil (a[5])
+		assert_equal (table.insert, a.insert)
+	end))
 
-ptable(a:keys())
-ptable(a:values())
-print(a:hasKey('x'))
-print(a:hasKey('xx'))
-print(a:isEmpty())
+end)
